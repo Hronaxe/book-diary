e0c38bb6eec301192957161a028abd7d148e6710
OrliogloDenis <orlioglodenis24@gmail.com>
2025-05-22 19:43:19 +0300

    add templates

--- users/views.py
+++ users/views.py
@@ -13,3 +13,30 @@ def register(request):
     else:
         form = UserRegisterForm()
     return render(request, 'register.html', {'form': form})
+
+
+# from django.shortcuts import render, redirect
+# from django.contrib import messages
+# from django.contrib.auth import login, authenticate
+# from .forms import UserRegisterForm
+
+# def register(request):
+#     if request.method == 'POST':
+#         form = UserRegisterForm(request.POST)
+#         if form.is_valid():
+#             user = form.save()  # Save user to DB
+#             username = form.cleaned_data.get('username')
+#             raw_password = form.cleaned_data.get('password1')  # Get raw password
+
+#             # Authenticate and login the user
+#             user = authenticate(username=username, password=raw_password)
+#             if user is not None:
+#                 login(request, user)
+#                 messages.success(request, f'Welcome, {username}!')
+#                 return redirect('home')
+#             else:
+#                 messages.warning(request, 'Authentication failed. Please log in manually.')
+#                 return redirect('login')
+#     else:
+#         form = UserRegisterForm()
+#     return render(request, 'register.html', {'form': form})
