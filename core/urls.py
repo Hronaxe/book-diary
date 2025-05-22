5a0c102e682652d634817cd7a6ced2b8be490869
Anatoli Burcenco <burcenko111@gmail.com>
2025-05-22 19:21:53 +0300

    add auth

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
+from users import views as user_views
 from . import views
+
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("register", views.registration, name = "register")
+    path("register", views.registration, name = "register"),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
+    path('profile/', user_views.profile, name='profile')
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .forms import UserRegisterForm
@@ -9,7 +10,11 @@ def register(request):
             form.save() # Save user to Database
             username = form.cleaned_data.get('username') # Get the username that is submitted
             messages.success(request, f'Account created for {username}!') # Show sucess message when account is created
-            return redirect('home') # Redirect user to Homepage
+            return redirect('login') # Redirect user to login
     else:
         form = UserRegisterForm()
     return render(request, 'register.html', {'form': form})
+
+@login_required # Require user logged in before they can access profile page
+def profile(request):
+    return render(request, 'users/profile.html')
